f5a16b7f2d4e6f43058c500acf0328bd045eeff5
Andre Antunes Vieira <andreantunesv@gmail.com>
2017-10-05 00:19:36 -0300

    First route

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
+
 app.listen(port, function () {
   console.log('Umbler listening on port %s', port);
 });
